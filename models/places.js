3050ec68eb619505e7f9e24d95d45da98e5d3043
Cesar BezZar17 <ccruz2408@outlook.com>
2023-06-09 14:29:37 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports= [{
+    name: 'Fortunate Son',
+    city: 'San Diego',
+    state: 'CA',
+    cuisines: 'Chinese Resturant',
+    pic: '/css/images/forson.jpg',
+  }, {
+    name: 'Soho House',
+    city: 'Los Angeles',
+    state: 'CA',
+    cuisines: 'Mixture',
+    pic: '/css/images/soho.jpg',
+}]
